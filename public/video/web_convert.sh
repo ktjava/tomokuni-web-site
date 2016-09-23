c328268a2f3f3f0b57aab8751a937f4b30c8c453
Tomokuni Kani <tomokuni.kani@gmail.com>
2016-09-24 03:40:34 +0900

    Change Opening

--- public/video/web_convert.sh
+++ public/video/web_convert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -i source/spa-shell-bg-video.mov -s 80x45 -sws_flags sinc spa-shell-bg-video.mov
+ffmpeg -i source/spa-shell-bg-video.mov -s 160x90 -sws_flags sinc spa-shell-bg-video.mov
